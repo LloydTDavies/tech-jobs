46a4592b2efee327c96d89faf652a82950986030
Lloyd Davies <lloydtdaviesit@gmail.com>
2023-11-10 20:20:26 +0000

    Added comment

--- apps/tech-jobs-pwa/src/app/app.routes.ts
+++ apps/tech-jobs-pwa/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { JobsComponent } from './jobs/jobs.component';
 
+// Need to have default route
 export const appRoutes: Route[] = [
     {
         path:'jobs',
